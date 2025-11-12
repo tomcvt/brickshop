1c67a5eb05eae6b23bf4efd496268d6907b0f16c
tomcvt <tomcvtdev@gmail.com>
2025-11-12 18:42:55 +0100

    fix: repository parameters fix

--- src/main/java/com/tomcvt/brickshop/repository/ShipmentRepository.java
+++ src/main/java/com/tomcvt/brickshop/repository/ShipmentRepository.java
@@ -11,14 +11,19 @@ import com.tomcvt.brickshop.enums.ShipmentStatus;
 import com.tomcvt.brickshop.model.Shipment;
 
 public interface ShipmentRepository extends JpaRepository<Shipment, Long> {
-    List<Long> findShipmentIdsByStatus(ShipmentStatus status, Pageable pageable);
+    @Query("""
+        SELECT s.id
+        FROM Shipment s
+        WHERE s.status = :status
+    """)
+    List<Long> findIdsByStatus(ShipmentStatus status, Pageable pageable);
 
     @Query("""
         SELECT s
         FROM Shipment s
         LEFT JOIN FETCH s.items si
         LEFT JOIN FETCH si.product
-        WHERE s.shipmentId IN :shipmentIds
+        WHERE s.id IN :ids
     """)
-    List<Shipment> findShipmentsWithItemsByIds(@Param("shipmentIds") List<Long> shipmentIds);
+    List<Shipment> findShipmentsWithItemsByIds(@Param("ids") List<Long> ids);
 }
