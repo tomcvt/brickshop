7b2942854ce1b754745332afac82668badfe776b
tomcvt <tomcvtdev@gmail.com>
2025-11-12 18:29:11 +0100

    format: deleting unused import

--- src/main/java/com/tomcvt/brickshop/controller/api/PackerApiController.java
+++ src/main/java/com/tomcvt/brickshop/controller/api/PackerApiController.java
@@ -10,8 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 import com.tomcvt.brickshop.dto.ShipmentDto;
 import com.tomcvt.brickshop.service.PackerService;
 
-import io.micrometer.core.ipc.http.HttpSender.Response;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
